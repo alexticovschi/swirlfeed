e9e0ecb93129e6c36d0a5ea9bedeb58e9d4b2be7
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-12 18:53:06 +0100

    getting values from the form and checking emails match

--- register.php
+++ register.php
@@ -1,3 +1,65 @@
+<?php  
+
+$con = mysqli_connect("localhost", "root", "root", "swirlfeed");
+
+if(mysqli_connect_errno()) {
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+// Declaring variables to prevent errors
+$fname = "";
+$lname = "";
+$em = "";
+$em2 = "";
+$password = "";
+$password2 = "";
+$date = "";
+$error_array = "";
+
+if(isset($_POST['register_button'])) {
+	// Registration form values
+
+	// First name
+	$fname = strip_tags($_POST['reg_fname']); // remove html tags
+	$fname = str_replace('', '', $fname); // remove spaces
+	$fname = ucfirst(strtolower($fname)); // uppercase first letter
+
+	// Last name
+	$lname = strip_tags($_POST['reg_lname']); // remove html tags
+	$lname = str_replace('', '', $lname); // remove spaces
+	$lname = ucfirst(strtolower($lname)); // uppercase first letter
+
+	// Email
+	$em = strip_tags($_POST['reg_email']); // remove html tags
+	$em = str_replace('', '', $em); // remove spaces
+	$em = ucfirst(strtolower($em)); // uppercase first letter
+
+	// Confirmation Email
+	$em2 = strip_tags($_POST['reg_email2']); // remove html tags
+	$em2 = str_replace('', '', $em2); // remove spaces
+	$em2 = ucfirst(strtolower($em2)); // uppercase first letter
+
+	// Password
+	$password = strip_tags($_POST['reg_password']); // remove html tags
+	// Confirmation Password
+	$password2 = strip_tags($_POST['reg_password2']); // remove html tags
+
+	$date = date("Y-m-d");
+
+	if($em == $em2) {
+		// Check if email is in valid format
+		if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
+			$em = filter_var($em, FILTER_VALIDATE_EMAIL);
+		} else {
+			echo "Invalid format";
+		}
+	} else {
+		echo "Emails don't match";
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +67,7 @@
 	<title>Welcome To Swirlfeed</title>
 </head>
 <body>
-	<form action="register.php">
+	<form action="register.php" method="POST">
 		<input type="text" name="reg_fname" placeholder="First Name" required>
 		<input type="text" name="reg_lname" placeholder="Last Name" required>
 		<br>
@@ -17,7 +79,7 @@
 		<br>
 		<input type="password" name="reg_password2" placeholder="Confirm Password" required>
 		<br>
-		<input type="submit" name="register_button"value="Register">
+		<input type="submit" name="register_button" value="Register">
 	</form>
 </body>
 </html>
