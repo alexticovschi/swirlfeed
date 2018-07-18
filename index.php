17347f87ef0a6e2b7c04d1d97668d504ab6b31b8
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-18 20:20:26 +0100

    screating the newsfeed column and styling the user details panel

--- index.php
+++ index.php
@@ -5,15 +5,25 @@
 			<a href="#">
 				<img src="<?php echo $user['profile_pic']; ?>" alt="profile pic" />
 			</a>
+			<div class="user_details_left_right">
+				<a href="#">
+					<?php echo $user['first_name'] . ' ' . $user['last_name']; ?>
+				</a>
+				<br>
 			
-			<a href="#">
-				<?php echo $user['first_name'] . ' ' . $user['last_name']; ?>
-			</a>
-			<br>
-			<?php 
-				echo "Posts: " . $user['num_posts'] . "<br>"; 
-				echo "Likes: " . $user['num_likes'];
-			?>
+				<?php 
+					echo "Posts: " . $user['num_posts'] . "<br>"; 
+					echo "Likes: " . $user['num_likes'];
+				?>
+			</div>
+		</div>
+		
+		<div class="main_column column">
+			<form class="post_form" action="index.php" method="POST">
+				<textarea name="post_text" id="post_text" placeholder="Got something to say?" cols="40" rows="3"></textarea>
+				<input type="submit" name="post" id="post_buttom" value="Post">
+				<hr>
+			</form>
 		</div>
 
 	</div>
