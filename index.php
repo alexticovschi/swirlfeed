7d89b28e0cd9ca38dac610a2fc011a84ebfe2144
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-18 18:47:34 +0100

    adding data to the user details panel

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
 			<a href="#">
 				<img src="<?php echo $user['profile_pic']; ?>" alt="profile pic" />
 			</a>
+			
+			<a href="#">
+				<?php echo $user['first_name'] . ' ' . $user['last_name']; ?>
+			</a>
+			<br>
+			<?php 
+				echo "Posts: " . $user['num_posts'] . "<br>"; 
+				echo "Likes: " . $user['num_likes'];
+			?>
 		</div>
 
 	</div>
